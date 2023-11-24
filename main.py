625fca4e265db5f110e6841ed0074ad11ed2219f
earlight <earlightny@gmail.com>
2023-11-24 10:30:23 -0500

    add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import statsmodels as sm
+import statsmodels.regression.linear_model as srl
+from statsmodels import api as a
+import matplotlib.pyplot as plt
+from matplotlib.dates import drange
+import datetime as dt
