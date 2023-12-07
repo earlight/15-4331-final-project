f2269b53cce2206ff06b6b757a44c3734d43d304
Richard Lin <earlight@dhcp-10-29-128-79.dyn.MIT.EDU>
2023-12-07 16:43:44 -0500

    remove unnecessary imports

--- tools.py
+++ tools.py
@@ -1,11 +1,4 @@
 import pandas as pd
-import numpy as np
-import statsmodels as sm
-import statsmodels.regression.linear_model as srl
-from statsmodels import api as a
-import matplotlib.pyplot as plt
-from matplotlib.dates import drange
-import datetime as dt
 
 # tickers of largest largest active mutual fund by AUM for each morningstar category
 MUTUAL_FUND_CATEGORIES = {
